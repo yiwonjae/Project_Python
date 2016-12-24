481f65b43f23b3320f034b5fa2d39d13dd5fd955
이원재 <mellowlee@naver.com>
2016-12-24 18:39:12 +0900

    거북이 이동

---
+++ PythonStudy/PythonStudy/Study01/PythonStudy01.py
@@ -0,0 +1,25 @@
+import turtle as t1
+
+t1.shape("turtle")   # shape를 Shape로 하면 문법 오류가 발생한다. 
+                    # 해당 부분은 주의해야한다. auto indent는 Shape를 권했다.
+
+
+t1.forward(50)       # 거북이를 이동한다.
+t1.left(120)
+
+t1.forward(50)
+
+t1.left(120)
+
+
+
+
+t1.right(90)         # 방향 선회
+
+
+
+
+
+
+print("hello")
+print("hello")
